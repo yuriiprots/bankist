5d409b5fdaec2212e899f41982e1837773597cfd
yuriiprots <uraprots7849@gmail.com>
2024-02-20 10:29:36 +0200

    create the `calcDisplaySummary` function

--- script.js
+++ script.js
@@ -3,14 +3,21 @@
 const labelWelcome = document.querySelector(".welcome");
 const labelBalance = document.querySelector(".balance__value");
 const labelDate = document.querySelector(".date");
+const labelSumIn = document.querySelector(".summary__value--in");
+const labelSumOut = document.querySelector(".summary__value--out");
+const labelSumInterest = document.querySelector(".summary__value--interest");
 
 const containerApp = document.querySelector(".app");
 const containerMovements = document.querySelector(".movements");
 
 const btnLogin = document.querySelector(".login__btn");
+const btnSort = document.querySelector(".btn--sort");
+const btnTransfer = document.querySelector(".form__btn--transfer");
 
 const inputLoginUsername = document.querySelector(".login__input--user");
 const inputLoginPin = document.querySelector(".login__input--pin");
+const inputTransferTo = document.querySelector(".form__input--to");
+const inputTransferAmount = document.querySelector(".form__input--amount");
 
 const account1 = {
   owner: "Yurii Prots",
@@ -25,6 +32,7 @@ const account2 = {
 };
 
 const accounts = [account1, account2];
+let currentAccount;
 
 btnLogin.addEventListener("click", (event) => {
   event.preventDefault();
@@ -44,9 +52,10 @@ const createUsernames = (function (accs) {
 function signIn(username, pin) {
   accounts.forEach((account) => {
     if (username === account.username && pin === account.pin) {
-      displayWelcomeMessage(account.owner);
+      currentAccount = account;
+      displayWelcomeMessage(currentAccount.owner);
       displayUI();
-      //updateUI(account);
+      updateUI(currentAccount);
       console.log("Login successful!");
     }
   });
@@ -63,14 +72,17 @@ const displayUI = () => (containerApp.style.opacity = 1);
 
 const updateUI = function (account) {
   updateCurrentDate();
-  calculateUserBalance(account.movements);
-  // displayMovements(account.movements);
+  calcDisplayBalance(account.movements);
+  displayMovements(account.movements);
+  calcDisplaySummary(account.movements);
 };
 
-const calculateUserBalance = function (movements) {
-  let userBalance = movements.reduce((acc, mov) => acc + mov, 0);
+const calcDisplayBalance = function (movements) {
+  const userBalance = movements.reduce((acc, mov) => acc + mov, 0);
+  currentAccount.balance = userBalance;
   labelBalance.textContent = `${userBalance} €`;
 };
+
 
 const updateCurrentDate = function () {
   const now = new Date();
@@ -81,3 +93,23 @@ const updateCurrentDate = function () {
   const minute = `${now.getMinutes()}`.padStart(2, 0);
   labelDate.textContent = `${day}/${month}/${year}, ${hour}:${minute}`;
 };
+
+const calcDisplaySummary = function (movements) {
+  const sumIn = movements
+    .filter((mov) => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${sumIn} €`;
+
+  const sumOut = movements
+    .filter((mov) => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(sumOut)} €`;
+
+  const interest = movements
+    .filter((mov) => mov > 0)
+    .map((deposit) => (deposit * 1.2) / 100)
+    .filter((int) => int >= 1)
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest} €`;
+};
+
