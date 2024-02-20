021839b7dc24d435e166c996fca93c6776880182
yuriiprots <uraprots7849@gmail.com>
2024-02-20 10:30:21 +0200

    create the `displayMovements` functions

--- script.js
+++ script.js
@@ -113,3 +113,17 @@ const calcDisplaySummary = function (movements) {
   labelSumInterest.textContent = `${interest} €`;
 };
 
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = "";
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? "deposit" : "withdrawal";
+    const row = `<div class="movements__row">
+          <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+          <div class="movements__value">${mov} €</div>
+        </div>`;
+    containerMovements.insertAdjacentHTML("afterbegin", row);
+  });
+};
+
